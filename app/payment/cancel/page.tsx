daf63cbbf8a6fe7a1e3dad5dd14be320b0c60425
Justin_Yamberla <justin.yamberla@epn.edu.ec>
2025-07-07 11:07:11 -0500

    Add
    Confirm and cancel pages

---
+++ app/payment/cancel/page.tsx
@@ -0,0 +1,9 @@
+export const CancellationPage = () => {
+    return (
+        <div className="min-vh-100 w-100 container">
+            Cancellation page
+        </div>
+    );
+}
+
+export default CancellationPage;
---
+++ app/payment/confirm/page.tsx
@@ -0,0 +1,9 @@
+export const ConfirmationPage = () => {
+    return (
+        <div className="min-vh-100 w-100 container">
+            Confirmation page
+        </div>
+    );
+}
+
+export default ConfirmationPage;
